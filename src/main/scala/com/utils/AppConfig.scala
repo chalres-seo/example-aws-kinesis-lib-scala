784886e0e1c3c8e762098d908517d45b09367bfc
charles <eng.charles.seo@gmail.com>
2018-07-13 11:53:00 +0900

    * Add License. * Edit application config.

--- src/main/scala/com/utils/AppConfig.scala
+++ src/main/scala/com/utils/AppConfig.scala
@@ -1,26 +1,37 @@
 package com.utils
 
+import java.io.File
+
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
 import com.amazonaws.services.kinesis.model.ShardIteratorType
 import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration
+import com.typesafe.config.{Config, ConfigFactory}
 
 object AppConfig {
-  // aws account config
-  val DEFAULT_AWS_PROFILE_NAME = "default"
-  val DEFAULT_AWS_REGION_NAME = "ap-northeast-2"
+  // read application.conf
+  private val conf: Config = ConfigFactory.parseFile(new File("conf/application.conf")).resolve()
 
-  // kinesis config
-  val DEFAULT_SHARD_COUNT = 1
-  val DEFAULT_INTERVAL_MILLIS = 1000L
-  val DEFAULT_CHECKPOINT_INTERVAL_MILLIS = 60000L
-  val DEFAULT_SHARD_ITERATOR_TYPE = ShardIteratorType.LATEST
-  val DEFAULT_KCL_INITIAL_STREAM_POSITION = InitialPositionInStream.TRIM_HORIZON
+  // common config
+  val DEFAULT_INTERVAL_MILLIS = conf.getLong("intervalMillis")
 
   // Retry config
-  val DEFAULT_ATTEMPT_COUNT = 10
-  val DEFAULT_BACKOFF_TIME_IN_MILLIS = 1000L
+  val DEFAULT_ATTEMPT_COUNT = conf.getInt("retry.attemptCount")
+  val DEFAULT_BACKOFF_TIME_IN_MILLIS = conf.getLong("retry.backoffTimeInMillis")
+
+  // aws account config
+  val DEFAULT_AWS_PROFILE_NAME = conf.getString("aws.profile")
+  val DEFAULT_AWS_REGION_NAME = conf.getString("aws.region")
+
+  // kinesis config
+  val DEFAULT_SHARD_COUNT = conf.getInt("aws.kinesis.shardCount")
+  val DEFAULT_SHARD_ITERATOR_TYPE = ShardIteratorType.valueOf(conf.getString("aws.kinesis.shardIteratorType"))
+
+  // kcl config
+  val DEFAULT_CHECKPOINT_INTERVAL_MILLIS = conf.getLong("aws.kcl.checkPointIntervalMillis")
+  val DEFAULT_KCL_INITIAL_STREAM_POSITION = InitialPositionInStream.valueOf(conf.getString("aws.kcl.initialStreamPosition"))
 
   // kpl config
   val DEFAULT_KPL_PROPS: KinesisProducerConfiguration = KinesisProducerConfiguration
-    .fromPropertiesFile("conf/aws_kinesis_kpl_default.properties")
+    .fromPropertiesFile(conf.getString("aws.kpl.daemonPropsPath"))
+
 }
